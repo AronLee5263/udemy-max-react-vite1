1d116c6bb0fd763042978be127aba62e299669db
AronLee5263 <aronlee5263@gmail.com>
2023-03-01 05:04:10 +0900

    State 올리기
    이유 : 이벤트 핸들러의 스크립트 위치와 업데이트 되는 상태의 위치가 다를때 props 이용하는 기법

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostList.module.css";
 
 function PostList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Maximiaan" body="the first props exercise!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manel" body="the second props pratice!" />
       </ul>
     </>
