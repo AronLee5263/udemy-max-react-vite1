ae205ba05ca7799282dcef0d4a469c764e0e243e
Aron <aronlee5263@gmail.com>
2023-03-02 21:18:06 +0900

    상태에 따라 모달창 turn on / off 구현

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,11 +3,10 @@ import classes from "./Modal.module.css";
 function Modal(props) {
   return (
     <>
-      <div className={classes.backdrop}>
-        <dialog open={true} className={classes.modal}>
-          {props.children}
-        </dialog>
-      </div>
+      <div className={classes.backdrop} onClick={props.onClose} />
+      <dialog open={true} className={classes.modal}>
+        {props.children}
+      </dialog>
     </>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -6,25 +6,30 @@ import Modal from "./Modal";
 import classes from "./PostList.module.css";
 
 function PostList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
   }
-
   function authorChangeHandler(event) {
     setEnteredAuthor(event.target.value);
   }
-
   return (
     <>
-      <Modal>
-        <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={authorChangeHandler}
-        />
-      </Modal>
+      {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      ) : null}
+
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manel" body="the second props pratice!" />
@@ -32,5 +37,4 @@ function PostList() {
     </>
   );
 }
-
 export default PostList;
