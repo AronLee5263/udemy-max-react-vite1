2891ccfb8e52e27a077875e873898b8397b45387
AronLee5263 <aronlee5263@gmail.com>
2023-03-05 00:29:47 +0900

    NewPost 컴포넌트에 summit 버튼 기능 추가
    1. summit 버튼 누를시 https 요청 보내는거 막기
    2. 모달창 닫기
    3. 사용자가 입력한 body, author 데이터 가져오기

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -6,29 +6,14 @@ import Modal from "./Modal";
 import classes from "./PostList.module.css";
 
 function PostList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
-
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manel" body="the second props pratice!" />
       </ul>
     </>
