e96fc6ee56e97a3bc2e981af280a4030907090ce
Aron gram <aronlee5263@gmail.com>
2023-03-13 20:01:57 +0900

    로딩 상태 관리
    백엔드 서버에서 데이터를 가져오거나 저장할떄 시간이 걸릴텐데
    사용자에게 로딩중이라는 걸 알려주는 코드

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -7,12 +7,15 @@ import classes from "./PostList.module.css";
 
 function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetchting, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -37,16 +40,23 @@ function PostList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        {posts.length > 0 &&
-          posts.map((post) => (
+      {!isFetchting && posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
-      </ul>
-      {posts.length === 0 && (
+        </ul>
+      )}
+
+      {!isFetchting && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>포스트가 없습니다.</h2>
           <p>여기에 내용을 추가해보세요 !</p>
+        </div>
+      )}
+      {isFetchting && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>게시물 로딩중...</p>
         </div>
       )}
     </>
