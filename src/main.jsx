1f03412b1b05358b761bc2bef8040db0c96f697d
Aron gram <aronlee5263@gmail.com>
2023-03-17 16:14:19 +0900

    NewPost 컴포넌트 라우트 경로 설정 및 추가 함.

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import NewPost from "./components/NewPost";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/create-post",
+    element: <NewPost />,
   },
 ]);
 
