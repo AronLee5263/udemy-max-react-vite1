42a58c63f0f44c84d5ff7ed20cfdb24b1c11f711
AronLee5263 <aronlee5263@gmail.com>
2023-03-19 21:03:16 +0900

    === 리팩토링 시작  ===
    - 메인 헤더 부분 중첩 에러 수정 진행중
    - app.jsx 이름 변경

---
+++ src/routes/Posts.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import PostList from "../components/PostList";
+
+function Posts() {
+  return (
+    <>
+      <main>
+        <PostList />
+      </main>
+    </>
+  );
+}
+
+export default Posts;
