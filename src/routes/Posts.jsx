4e66e12f1c560950a806b28a4f04bcdeda3290db
Aron gram <aronlee5263@gmail.com>
2023-03-23 21:50:23 +0900

    loader 함수 리턴 받은 값 로그찎기

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -15,7 +15,9 @@ function Posts() {
 export default Posts;
 
 export async function loader() {
+  console.log("로더 함수가 호출 되면 밑에 객체 로그찍힐겁니다");
   const response = await fetch("http://localhost:8080/posts");
   const resData = await response.json();
+  console.log(resData);
   return resData.Posts;
 }
